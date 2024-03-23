178cd8cf58e310e1bd7378b5034cfbc551f8fedd
Aliaa111214 <152916832+Aliaa111214@users.noreply.github.com>
2024-03-23 22:21:20 +0200

    Update while.py change variable name

--- while.py
+++ while.py
@@ -1,8 +1,8 @@
 print(" this program will continue to ask you to enter a number untill you enter -1 then it will calculate the average of the numbers you enterd ! ")
 number=int(input("please enter a number\n"))
-sum=number
+sum=number_input
 i=1
-while number!=-1:
+while number_input!=-1:
  number=int(input("please enter a number\n"))
 # the if condition is important as i noticed while runing the progarm that it takes -1 into calculation if this condition is not there
  if number==-1:
