a86f8f8de15a2f42fb3ae097f09e110ef6e9e782
Aliaa111214 <152916832+Aliaa111214@users.noreply.github.com>
2024-03-23 22:14:45 +0200

    Add files via upload

---
+++ while.py
@@ -0,0 +1,15 @@
+print(" this program will continue to ask you to enter a number untill you enter -1 then it will calculate the average of the numbers you enterd ! ")
+number=int(input("please enter a number\n"))
+sum=number
+i=1
+while number!=-1:
+ number=int(input("please enter a number\n"))
+# the if condition is important as i noticed while runing the progarm that it takes -1 into calculation if this condition is not there
+ if number==-1:
+  break
+ sum=sum+number
+ i+=1
+print(sum)    
+print("i="+str(i))
+average=sum/i
+print("the averge of the numbers you entered is "+str(average)+".")
