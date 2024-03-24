0e3b148aea48a37dce170bf62585565e32d4a507
Aliaa111214 <152916832+Aliaa111214@users.noreply.github.com>
2024-03-24 06:08:34 +0200

    Update while.py chane variable name to number input

--- while.py
+++ while.py
@@ -1,5 +1,5 @@
 print(" this program will continue to ask you to enter a number untill you enter -1 then it will calculate the average of the numbers you enterd ! ")
-number=int(input("please enter a number\n"))
+number_input=int(input("please enter a number\n"))
 sum=number_input
 i=1
 while number_input!=-1:
